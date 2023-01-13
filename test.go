a2490c978ed56277821b0c75aec08789a2ff4fd5
chenna kesava kumar <88958075+chedevops57@users.noreply.github.com>
2023-01-13 12:40:52 +0530

    test
    check remote

---
+++ test.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to the conference booking app")
+	fmt.Println("Get your tickets here to attend")
+	var conferenceName = "Go conference"
+	fmt.Println(conferenceName)
+}
